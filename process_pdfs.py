4b6c0b307ed29a5fecbd3bc76a39f21c75586f3c
Curtis Hendricks <curtis.hendricks@gmail.com>
2020-05-18 13:30:17 -0400

    Removed uncessary commented out old text

--- process_pdfs.py
+++ process_pdfs.py
@@ -70,7 +70,6 @@ def process_pdf_oral_q(oral_q_section, question_head_ptrn, speaker_ptrn,
                             quest_dialog_list, data_type='list')
     # repair titles
     new_list = []
-    add_to = ''
     # repair speakers titles
     counter = 0
     for idx in range(len(quest_dialog_list)):
@@ -83,15 +82,6 @@ def process_pdf_oral_q(oral_q_section, question_head_ptrn, speaker_ptrn,
                 quest_dialog_list[idx-2] + quest_dialog_list[idx-1])
             new_list.append(quest_dialog_list[idx] + quest_dialog_list[idx+1])
 
-    # for item in quest_dialog_list:
-    #     if item in SPEAKER_TITLES:
-    #         add_to = item
-    #     else:
-    #         if add_to:
-    #             new_list.append(add_to + item)
-    #             add_to = ''
-    #         else:
-    #             new_list.append(item)
     quest_dialog_list = new_list
     utils.send_text_to_file('NWT/tmp/'+str_date+'fixed_oral_questions_list.txt',
                             quest_dialog_list, data_type='list')
@@ -114,56 +104,3 @@ def process_pdf_oral_q(oral_q_section, question_head_ptrn, speaker_ptrn,
     utils.csv_from_list(csv_name, speakers_table,
                         header_row=['Question', 'Speaker', 'Speech'])
     pdfs_logger.debug('Created CSV file: %s' % csv_name)
-
-
-# if __name__ == '__main__':
-#     oral_ques_pattern = r'(Question\s+\d{1,3}.*?:.*?)\s*(M[R|S]S{0,1}\.|HON\.|HONOURABLE)'
-#     speaker_pattern = r'((?:M[R|S]S{0,1}\.|HON\.|HONOURABLE).*?):'
-
-#     with open('NWT/oral_ques_sec_01.txt', 'r') as f:
-#         str1 = f.read()
-
-#     lst = text_split(str1, oral_ques_pattern)
-#     print('List length:', len(lst))
-#     print(lst[0])
-#     print(lst[1])
-#     print(lst[2])
-#     print(lst[3])
-#     new_list = []
-#     add_to = ''
-#     for item in lst:
-#         if item in TITLES:
-#             add_to = item
-#         else:
-#             if add_to:
-#                 new_list.append(add_to + item)
-#                 add_to = ''
-#             else:
-#                 new_list.append(item)
-#     print('New list:', len(new_list))
-#     speakers_table = []
-#     for item in new_list:
-#         if item[:8] == 'Question':
-#             loc1 = item
-#         else:
-#             speech_lst = text_split(item, speaker_pattern)
-#             loc2 = ''
-#             loc3 = ''
-#             for speech in speech_lst:
-#                 if speech.split(' ')[0] in TITLES:
-#                     loc2 = speech
-#                 else:
-#                     loc3 = speech
-#                     speakers_table.append((loc1, loc2, loc3))
-
-#     for row in speakers_table:
-#         print(row[1])
-
-    # print(new_list[0])
-    # print(new_list[1])
-    # print(new_list[2])
-    # print(new_list[3])
-    # new_list_odd = new_list[::2]
-    # new_list_even = new_list[1::2]
-    # print('Length odd:', len(new_list_odd))
-    # print('Length even:', len(new_list_even))
